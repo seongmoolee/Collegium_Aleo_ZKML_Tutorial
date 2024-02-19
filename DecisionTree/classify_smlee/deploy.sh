dd43d771a81d7d1ad634b564c12de97fd6647458
seongmoolee <seongmoolee@gmail.com>
2024-02-20 02:24:01 +0900

    deploy & execute

--- DecisionTree/classify_smlee/deploy.sh
+++ DecisionTree/classify_smlee/deploy.sh
@@ -1,10 +1,11 @@
 APPNAME="classify_smlee"
-ENV_FILE=".env"
-PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+#ENV_FILE=".env"
+#PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+PRIVATE_KEY=""
 
 snarkos developer deploy "${APPNAME}.aleo" \
   --path "./build/" \
   --private-key "${PRIVATE_KEY}" \
-  --query "<https://api.explorer.aleo.org/v1>" \
+  --query "https://api.explorer.aleo.org/v1" \
   --priority-fee 3000000 \
-  --broadcast "<https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast>" \
+  --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" \
--- DecisionTree/classify_smlee/run.sh
+++ DecisionTree/classify_smlee/run.sh
@@ -1,12 +1,11 @@
 APPNAME="classify_smlee"
-ENV_FILE=".env"
-PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+#ENV_FILE=".env"
+#PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+PRIVATE_KEY=""
 
-snarkos developer execute "${APPNAME}.aleo"  main \
-e0:0u32 \
-e1:0u32 \ 
-  --path "./build/" \
+snarkos developer execute "${APPNAME}.aleo" main \
+  '{e0:0u32, e1:0u32}' \
   --private-key "${PRIVATE_KEY}" \
-  --query "<https://api.explorer.aleo.org/v1>" \
+  --query "https://api.explorer.aleo.org/v1" \
   --priority-fee 3000000 \
-  --broadcast "<https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast>" \
+  --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" \
